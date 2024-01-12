78a4868edb6a5a8279e1d0b68ad4403da1f6f4f7
Paolo <64132836+pcavad@users.noreply.github.com>
2024-01-12 11:38:19 +0100

    First upload

---
+++ data/datasetup.py
@@ -0,0 +1,13 @@
+"""
+This is a list of exceptions which the utils.load_data function will use to update the data.
+"""
+
+_exceptions = [
+    'CONDOMINIO VIA SETTEMBRINI'
+    , 'PAYPAL *UNIEURO S P 35314369001'
+    , 'PAYPAL *MEDIAMARKET 35314369001'
+    , 'PAYPAL *ALOISDALLMA 35314369001'
+    , 'PAYPAL *ALOISDALLMA 892135130'
+    , 'IKEA'
+    , 'BANCOMAT ATM'
+]
